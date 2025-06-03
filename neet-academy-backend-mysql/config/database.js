b6598535113f1905bb1c43868b5f6bbdc02b58bf
somashekhar <somashekharnaikn3@gmail.com>
2025-06-03 10:15:49 +0530

    Change env13

--- neet-academy-backend-mysql/config/database.js
+++ neet-academy-backend-mysql/config/database.js
@@ -1,5 +1,4 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,7 +7,13 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false, // optional
   }
 );
 

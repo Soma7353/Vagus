4709133e4ec91cffdcac36a497af0ab79f5540f8
somashekhar <somashekharnaikn3@gmail.com>
2025-06-05 00:59:55 +0530

    commit-38

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
     ca: fs.readFileSync(path.join(__dirname, 'isrgrootx1.pem')) // Make sure ca.pem is in /config

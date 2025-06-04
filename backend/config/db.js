aecde9f03fe71a4fda0f0ef15df0bf938ad100aa
somashekhar <somashekharnaikn3@gmail.com>
2025-06-05 00:44:09 +0530

    commit-36

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql2/promise');
+const fs = require('fs');
+const path = require('path');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const pool = mysql.createPool({
@@ -7,6 +10,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, 'isrgrootx1.pem')) // Make sure ca.pem is in /config
+  }
 });
 
 module.exports = pool;

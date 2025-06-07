5ed125cc88008d48928d4aeca8429d9d3aa389ee
somashekhar <somashekharnaikn3@gmail.com>
2025-06-08 01:24:34 +0530

    commit-121

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,8 +33,8 @@ function App() {
           element={
             <>
               <main className="pt-24">
-                <Hero />
                 <Slider />
+                <Hero />
                 <Courses />
                 <Features />
                 <Results />

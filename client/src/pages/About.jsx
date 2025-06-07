23ab798c281f520ddd0564a35c8efd61cb79eaa7
somashekhar <somashekharnaikn3@gmail.com>
2025-06-07 11:42:32 +0530

    commit-100

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -48,18 +48,18 @@ const About = () => {
           </div>
 
           {/* Image or YouTube Embed */}
-          <div className="flex justify-center">
-  <div className="w-full max-w-4xl aspect-video shadow-xl rounded-lg overflow-hidden">
-    <iframe
-      className="w-full h-full"
-      src="https://www.youtube.com/embed/uQzFihB5-B4"
-      title="Director's Message"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    ></iframe>
-  </div>
-</div>
+          <div className="rounded-lg overflow-hidden shadow-lg">
+            <iframe
+              width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/uQzFihB5-B4"
+              title="Director's Message"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className="w-full h-full"
+            ></iframe>
+          </div>
+        </div>
 
         {/* Bottom Card Grid */}
         <div className="text-center mb-8">
